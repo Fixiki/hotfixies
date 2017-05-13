e4b5f6834cab6e14562991ae91be088c1c3327b7
Hardkun <9alexua9@gmail.com>
2017-05-13 23:40:10 +0300

    Minor fixies

--- neural_net_backend/temp.py
+++ neural_net_backend/temp.py
@@ -1,9 +1,5 @@
-
-def rand_dom(num):
-	return num**2
-
-
-def test_function(X_train, y_train, X_test):
-	"some"+"advanced"+"calculations"
-	return [0]*len(X_test)
-
+def evaluate_algo(X_train, y_train, X_test):
+	"""
+	Пример функции для предсказания тестовых данных по обучающим
+	"""	
+	return [0]*len(X_test)#y_test estimation
